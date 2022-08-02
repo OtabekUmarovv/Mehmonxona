fd228af394495648d95882d2920d2bdb6ed31dba
Dastanbayev02 <dastanbayev02s@gmail.com>
2022-08-02 13:31:34 +0500

    Data yozildi

--- Mehmonxona.Domain/Entities/Rooms/Room.cs
+++ Mehmonxona.Domain/Entities/Rooms/Room.cs
@@ -1,13 +1,20 @@
 ﻿
 using Mehmonxona.Domain.Commons;
 using Mehmonxona.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Mehmonxona.Domain.Entities.Rooms
 {
     public class Room : Auditable
     {
+        [Required, MaxLength(64)]
         public string Sign { get; set; }
         public RoomType Type { get; set; }
-        public RoomStatus Status { get; set; } = RoomStatus.Free;
+        public RoomStatus Status { get; set; }
     }
 }
